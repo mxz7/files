f8c59b4865f8959fef80534b71e154bd475c6ae9
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-04-20 15:25:32 +0100

    don't error bc undefined

--- src/lib/server/database/db.ts
+++ src/lib/server/database/db.ts
@@ -2,7 +2,10 @@ import { env } from "$env/dynamic/private";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-const client = createClient({ authToken: env.DB_TOKEN, url: env.DB_URL });
+const client = createClient({
+  authToken: env.DB_TOKEN || "meow meow meow", // errors during build
+  url: env.DB_URL || "libsql://meow-meow-meow.turso.io", // errors during build
+});
 
 const db = drizzle(client);
 
