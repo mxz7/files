7b5b1a4b0da2ff9032aa088b18f40ff5a541c58c
max <29782397+mxz7@users.noreply.github.com>
2025-11-20 09:47:36 +0000

    clean up usage of exiftool-vendored?

--- src/lib/server/exif.ts
+++ src/lib/server/exif.ts
@@ -1,24 +1,10 @@
-import { ExifTool } from "exiftool-vendored";
+import { exiftool } from "exiftool-vendored";
 import { readFile, writeFile } from "node:fs/promises";
-
-let inUse = false;
 
 export async function stripExif(
   file: File,
   id: string,
 ): Promise<{ success: true; file: Buffer } | { success: false }> {
-  if (inUse) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(stripExif(file, id));
-      }, 100);
-    });
-  }
-
-  console.log("[exif] initating");
-
-  inUse = true;
-  const exiftool = new ExifTool();
   try {
     console.log("[exif] saving file");
     await writeFile(`/tmp/${encodeURIComponent(id)}`, Buffer.from(await file.arrayBuffer()));
@@ -33,16 +19,9 @@ export async function stripExif(
     console.log("[exif] ending");
     await exiftool.end();
 
-    inUse = false;
-
     return { success: true, file: await readFile(`/tmp/${encodeURIComponent(id)}`) };
   } catch (e) {
-    inUse = false;
     console.error(e);
-    await exiftool.end();
     return { success: false };
-  } finally {
-    await exiftool.end();
-    inUse = false;
   }
 }
