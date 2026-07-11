050bef9de88e847ac4b51bf4eb39158877bf1ba2
max <29782397+mxz7@users.noreply.github.com>
2026-07-11 16:33:37 +0100

    fix: only check s3 during runing

--- src/lib/server/s3.ts
+++ src/lib/server/s3.ts
@@ -1,10 +1,13 @@
+import { building } from "$app/environment";
 import { env } from "$env/dynamic/private";
 import { S3Client } from "@aws-sdk/client-s3";
 
 const { S3_ENDPOINT, S3_REGION, S3_KEY_ID, S3_KEY } = env;
 
-if (!S3_ENDPOINT || !S3_REGION || !S3_KEY_ID || !S3_KEY) {
-  throw new Error("Missing required S3 configuration");
+if (!building) {
+  if (!S3_ENDPOINT || !S3_REGION || !S3_KEY_ID || !S3_KEY) {
+    throw new Error("Missing required S3 configuration");
+  }
 }
 
 const s3 = new S3Client({
