f6f9e65d0a73c9e6243c09566e13d27a419a23e5
max <29782397+mxz7@users.noreply.github.com>
2026-07-12 15:18:02 +0100

    fix(s3)?: tell ts it exists?

--- src/lib/server/s3.ts
+++ src/lib/server/s3.ts
@@ -2,12 +2,12 @@ import { env } from "$env/dynamic/private";
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  endpoint: env.S3_ENDPOINT,
+  endpoint: env.S3_ENDPOINT!,
   region: "auto",
   requestChecksumCalculation: "WHEN_REQUIRED",
   responseChecksumValidation: "WHEN_REQUIRED",
   credentials: {
-    accessKeyId: env.S3_KEY_ID,
-    secretAccessKey: env.S3_ACCESS_KEY,
+    accessKeyId: env.S3_KEY_ID!,
+    secretAccessKey: env.S3_ACCESS_KEY!,
   },
 });
